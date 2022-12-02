ff183d7beb3ef5d36a1f47c412ae92c256052fd6
damiankrajnak <damian.krajnakk@gmail.com>
2022-12-02 21:06:26 +0100

    Add day02 solution

---
+++ day02/app.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+
+const scoreMapper = {
+  A: {
+    X: 4,
+    Y: 8,
+    Z: 3,
+  },
+  B: {
+    X: 1,
+    Y: 5,
+    Z: 9,
+  },
+  C: {
+    X: 7,
+    Y: 2,
+    Z: 6,
+  },
+};
+
+const myTurnMapper = {
+  A: {
+    X: "Z",
+    Y: "X",
+    Z: "Y",
+  },
+  B: {
+    X: "X",
+    Y: "Y",
+    Z: "Z",
+  },
+  C: {
+    X: "Y",
+    Y: "Z",
+    Z: "X",
+  },
+};
+
+const part1 = fs
+  .readFileSync("input.txt")
+  .toString()
+  .split("\n")
+  .map((match) => scoreMapper[match.split(" ")[0]][match.split(" ")[1]])
+  .reduce((a, b) => a + b, 0);
+
+const part2 = fs
+  .readFileSync("input.txt")
+  .toString()
+  .split("\n")
+  .map(
+    (match) =>
+      scoreMapper[match.split(" ")[0]][
+        myTurnMapper[match.split(" ")[0]][match.split(" ")[1]]
+      ]
+  )
+  .reduce((a, b) => a + b, 0);
+
+console.log(`PART1: ${part1}\nPART2: ${part2}`);
