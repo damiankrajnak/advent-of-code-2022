31f294ed8b74531f07531d30db4b91f61eaada8a
damiankrajnak <damian.krajnakk@gmail.com>
2022-12-06 14:11:28 +0100

    Add day05 solution

---
+++ day05/part2.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+
+const [drawing, moves] = fs.readFileSync("input.txt").toString().split("\n\n");
+
+const numberOfCrates = Number(
+  drawing.split("\n").at(-1).trim().split("   ").at(-1)
+);
+
+const cratesIndexes = {};
+for (let i = 0; i < numberOfCrates; i++) {
+  cratesIndexes[i + 1] = i * 4 + 1;
+}
+
+const columnsOfCrates = drawing.split("\n");
+columnsOfCrates.pop();
+columnsOfCrates.reverse();
+
+const crates = {};
+
+for (let i = 1; i < 10; i++) {
+  crates[i] = [];
+}
+
+columnsOfCrates.forEach((row) => {
+  for (let i in cratesIndexes) {
+    if (row[cratesIndexes[i]] !== " ") {
+      crates[i].push(row[cratesIndexes[i]]);
+    }
+  }
+});
+
+// PART 2
+moves.split("\n").forEach((move) => {
+  const quantity = move.split(" ")[1];
+  const from = move.split(" ")[3];
+  const to = move.split(" ")[5];
+  const popped = [];
+  for (let i = 0; i < quantity; i++) {
+    popped.push(crates[from].pop());
+  }
+  crates[to].push(...popped.reverse());
+});
+
+let message = "";
+
+for (let crate in crates) {
+  if (crates[crate].length) {
+    message += crates[crate].at(-1);
+  }
+}
+
+console.log(message);
